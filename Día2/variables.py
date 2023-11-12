0fcf2a99acd7249ed591009509ea45a7e97aac0a
GeorGex3x383 <gjorgeemilio@gmail.com>
2023-11-12 15:28:57 -0600

    Agregando archivos del dia 2

---
+++ Día2/variables.py
@@ -0,0 +1,18 @@
+nombre = "Jorge"
+print(nombre)
+nombre = "Javi"
+print(nombre)
+
+edad = 24
+print(edad)
+edad2 = 23
+print(edad+edad2)
+
+nombreUsuario = input("Dime tu nombre: ")
+print("Tu nombre es "+nombreUsuario)
+
+texto1 = "Hola "
+texto2 = "Mundo"
+frase = texto1+texto2
+print(texto1+texto2)
+print(frase)
